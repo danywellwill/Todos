398b747316688eccad0eaa2287b77d7d7a112ec3
danywellwill <45115174+danywellwill@users.noreply.github.com>
2018-11-25 22:05:55 -0200

    testando o commit da build em /docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; 
+import { RouterModule, Routes } from '@angular/router'; 
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component'; 
+import { FormsModule } from '@angular/forms'; 
+
+const routes: Routes = [
+  { path: ':status', component: TodoComponent }, 
+  { path: '**', redirectTo: '/all' }
+]; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent 
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule, 
+    RouterModule.forRoot(routes) 
   ],
   providers: [],
   bootstrap: [AppComponent]
